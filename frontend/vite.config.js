e821282fc6126fbfa737570dc24b1290f9e8cccf
OverLord <overlordofgames@mail.ru>
2026-06-01 18:00:35 +0500

    Всё сломали, но все исправили

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
+    host: '127.0.0.1', // Принудительно запускаем на 127.0.0.1
+    port: 5173,        // Оставляем порт 5173
   }
 })
+
