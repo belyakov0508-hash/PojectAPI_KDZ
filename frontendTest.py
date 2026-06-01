ca52d296e89bf39e8b511ccecefe869e8e84053c
OverLord <overlordofgames@mail.ru>
2026-06-01 18:13:16 +0500

    Удаляю

--- frontendTest.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-if __name__ == "__main__":
-    print("Запускаем сборку фронтенда...")
-
-    try:
-        subprocess.run("npm run dev", shell=True, cwd="frontend", check=True)
-    except KeyboardInterrupt:
-        print("\nФронтенд успешно остановлен.")
